1c14bd414e4e2397d2003ec5fee8ca45bb475ec7
Jordan Reed <jordanreed03@gmail.com>
2020-11-27 18:43:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+ arrofnames = [ ]
+attendees.each do |name|
+  arrofnames.push(badge_maker(name))
+end
+return arrofnames
+end
+
+def assign_rooms(speakers)
+  roomarr = []
+   count = 1 
+  speakers.each do |name|
+  roomarr.push("Hello, #{name}! You'll be assigned to room #{count}!")
+  count += 1 
+end 
+return roomarr
+end 
+
+def printer(attendees)
+ batch_badge_creator(attendees).each do |ba|puts ba}
+
+end 
